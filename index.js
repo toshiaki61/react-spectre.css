7b78988a26d2548d482cd2dc867088c41fef151b
toshiaki saeki <toshiaki61@gmail.com>
2018-10-24 07:58:41 +0900

    to typescript

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test') {
-  module.exports = require('./dist/react-spectre.css.min')
-} else {
-  module.exports = require('./lib/index')
-}
+module.exports = require('./dist/index')
